7c8283a7f0ad6edaf52ff40921c5951d4cab5724
Cossack888 <67826646+Cossack888@users.noreply.github.com>
2024-01-27 11:35:43 +0100

    Changes in player controler

--- Assets/PlayerControllerNova.cs
+++ Assets/PlayerControllerNova.cs
@@ -1,5 +1,7 @@
+using DragonBones;
 using System.Collections;
 using System.Collections.Generic;
+using TreeEditor;
 using UnityEditor;
 using UnityEngine;
 
@@ -8,10 +10,12 @@ public class PlayerControllerNova : MonoBehaviour
     Vector3 direction;
     Rigidbody2D rb;
     [SerializeField] float speed;
+    UnityArmatureComponent armature;
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        armature = GetComponent<UnityArmatureComponent>();
     }
 
     // Update is called once per frame
@@ -20,8 +24,19 @@ public class PlayerControllerNova : MonoBehaviour
         float moveHorizontal = Input.GetAxis("Horizontal");
         float moveVertical = Input.GetAxis("Vertical");
 
-        direction = new Vector2(moveHorizontal, moveVertical);
+        direction = new Vector3(moveHorizontal, moveVertical, 0);
         direction = transform.TransformDirection(direction);
+        if (direction != Vector3.zero && !armature.animation.isPlaying)
+        {
+            armature.animation.Play("animtion0");
+        }
+        else if (direction == Vector3.zero)
+        {
+            armature.animation.Stop();
+        }
+        armature.sortingOrder = (int)Mathf.Floor(-transform.position.y);
+
+
     }
     private void FixedUpdate()
     {
