79980a94f753bffdc8ca03a7394435ac6f2076af
Mikołaj Twardowski <mtwardowski98@gmail.com>
2024-01-27 11:13:32 +0100

    Fix po zmianie projektu -> poruszania input system, dodanie Singletona do wywoływania Audio

--- Assets/Scripts/Player/Movement.cs
+++ Assets/Scripts/Player/Movement.cs
@@ -1,22 +1,25 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class Movement : MonoBehaviour
 {
-    CharacterController controller;
+    Rigidbody2D rb;
     [SerializeField] float speed = 10f;
     Vector3 horizontalInput;
 
     private void Awake()
     {
-        controller = GetComponent<CharacterController>();
+        rb = GetComponent<Rigidbody2D>();
     }
 
-    private void Update()
+    private void FixedUpdate()
     {
-        Vector3 hoizontalVelocity = (transform.right * horizontalInput.x + transform.forward * horizontalInput.y) * speed;
-        controller.Move(hoizontalVelocity * Time.deltaTime);
+        Vector3 horizontalVelocity = (transform.right * horizontalInput.x + transform.up * horizontalInput.y) * speed;
+        //horizontalVelocity.x += transform.position.x;
+        //horizontalVelocity.y += transform.position.y;
+        rb.MovePosition(transform.position + horizontalVelocity * Time.deltaTime);
     }
 
     public void ReceiveInput(Vector2 _horizontalInput)
